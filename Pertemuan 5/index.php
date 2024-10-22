9b909211b4847d12b069d92e97e2000293982cfb
ValeriaUkat <ukatvaleria17@gmail.com>
2024-10-22 09:03:25 +0700

    Update index.php

--- Pertemuan 5/index.php
+++ Pertemuan 5/index.php
@@ -1,1 +1,19 @@
+<?php
+session_start();
+?>
 
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Taskly Organize</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome to Taskly Organize</h1>
+        <a href="list-tugas.php">View Assignments</a> |
+        <a href="form-tambah.php">Add New Task</a> | 
+        <a href="dashboard.php">Dashboard</a>
+    </header>
+</body>
+</html>
