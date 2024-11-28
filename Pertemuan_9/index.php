79097bea024139735279dd7e3fe1e14c28f44c8f
bellatefa1392 <154943486+bellatefa1392@users.noreply.github.com>
2024-11-28 22:06:53 +0700

    Add files via upload

---
+++ Pertemuan_9/index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Taskly Organize</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Link ke file CSS -->
+</head>
+<body>
+    <?php include 'menu.php'; ?>
+    <header>
+        <h1>Welcome to Taskly Organize</h1>
+    </header>
+</body>
+</html>
---
+++ Pertemuan_9/proses-edit.php
@@ -0,0 +1,21 @@
+<?php
+include 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['id'];
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $due_date = $_POST['due_date'];
+    $priority = $_POST['priority'];
+
+    $sql = "UPDATE tugas SET title='$title', description='$description', due_date='$due_date', priority='$priority' WHERE task_id='$id'";
+
+    if ($conn->query($sql) === TRUE) {
+        header("Location: list-tugas.php");
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
