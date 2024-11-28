b8e7e3c795947cee651644b31901c9b7624059a0
AnggiNurlette <anggriani.nurlette@students.utdi.ac.id>
2024-11-28 21:07:47 +0700

    Update proses-tambah.php

--- Pertemuan 5/proses-tambah.php
+++ Pertemuan 5/proses-tambah.php
@@ -1,17 +1,20 @@
 <?php
 include 'config.php';
 
-$title = $_POST['title'];
-$description = $_POST['description'];
-$due_date = $_POST['due_date'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $due_date = $_POST['due_date'];
+    $priority = $_POST['priority'];
 
-$sql = "INSERT INTO tugas (title, description, due_date, status) VALUES ('$title', '$description', '$due_date', 'Belum Selesai')";
+    $sql = "INSERT INTO tugas (title, description, due_date, priority, status) VALUES ('$title', '$description', '$due_date', '$priority', 'Belum Selesai')";
 
-if ($conn->query($sql) === TRUE) {
-    header("Location: list-tugas.php");
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    if ($conn->query($sql) === TRUE) {
+        header("Location: list-tugas.php");
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
 }
-
-$conn->close();
 ?>
